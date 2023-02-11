1995e62ee10c96e2a7ca2b72bce20629151fc951
yukilun <102889935+yukilun@users.noreply.github.com>
2023-02-11 00:10:19 -0800

    update PWA

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.scss';
 import { AuthContextProvider } from './context/AuthContext';
 import { ChatContextProvider } from './context/ChatContext.js';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
@@ -16,3 +17,8 @@ root.render(
     </ChatContextProvider>
   </AuthContextProvider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
